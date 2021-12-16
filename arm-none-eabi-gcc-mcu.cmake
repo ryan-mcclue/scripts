7850b54e407299dc07a8dc19aec70fab6c6215e3
Ryan McClue <re.mcclue@protonmail.com>
2021-12-16 16:16:14 +1100

    Add CMake toolchain file for cross compiling libraries

---
+++ arm-none-eabi-gcc-mcu.cmake
@@ -0,0 +1,36 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_COMPILER    arm-none-eabi-gcc) 
+set(CMAKE_CXX_COMPILER  arm-none-eabi-g++)
+set(AS                  arm-none-eabi-as)
+set(CMAKE_AR            arm-none-eabi-gcc-ar)
+set(OBJCOPY             arm-none-eabi-objcopy)
+set(OBJDUMP             arm-none-eabi-objdump)
+set(SIZE                arm-none-eabi-size)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CPU_NAME cortex-m4)
+
+set(CMAKE_C_FLAGS_INIT
+  "-mcpu=cortex-m4 -mthumb -mfloat-abi=soft -fdata-sections -ffunction-sections"
+	CACHE
+	INTERNAL "Default C compiler flags.")
+set(CMAKE_CXX_FLAGS_INIT
+  "-mcpu=cortex-m4 -mthumb -mfloat-abi=soft -fdata-sections -ffunction-sections"
+	CACHE
+	INTERNAL "Default C++ compiler flags.")
+set(CMAKE_ASM_FLAGS_INIT
+	"-x assembler-with-cpp"
+	CACHE
+	INTERNAL "Default ASM compiler flags.")
+set(CMAKE_EXE_LINKER_FLAGS_INIT
+  "-Wl, --gc-sections"
+	CACHE
+	INTERNAL "Default linker flags.")

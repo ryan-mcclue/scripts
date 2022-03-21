3f3f734563136ab5ee63e57ea00b876bc4ccbad7
Ryan McClue <re.mcclue@protonmail.com>
2022-03-21 15:20:05 +1100

    Add file information in breakpoint message

--- tbc/desktop.cpp
+++ tbc/desktop.cpp
@@ -22,29 +22,29 @@ typedef int64_t  s64;
 typedef float r32;
 typedef double r64;
 
-#if defined(HHF_INTERNAL)
-  INTERNAL void __bp_non_debugger(char const *func_name, int line_num)
+#if defined(RAY_INTERNAL)
+  INTERNAL void __bp_non_debugger(char const *file_name, char const *func_name, int line_num)
   { 
-    fprintf(stderr, "BREAKPOINT TRIGGERED! (%s:%d)\n", func_name, line_num);
+    fprintf(stderr, "BREAKPOINT TRIGGERED! (%s:%s:%d)\n", file_name, func_name, line_num);
     exit(1);
   }
-  INTERNAL void __bp_debugger(char const *func_name, int line_num) { return; }
-  GLOBAL void (*__bp)(char const *, int) = __bp_non_debugger;
+  INTERNAL void __bp_debugger(char const *file_name, char const *func_name, int line_num) { return; }
+  GLOBAL void (*__bp)(char const *, char const *, int) = __bp_non_debugger;
 
-  INTERNAL void __ebp_non_debugger(char const *func_name, int line_num)
+  INTERNAL void __ebp_non_debugger(char const *file_name, char const *func_name, int line_num)
   { 
-    fprintf(stderr, "ERRNO BREAKPOINT TRIGGERED! (%s:%d)\n\"%s\"\n", func_name, line_num, strerror(errno));
+    fprintf(stderr, "ERRNO BREAKPOINT TRIGGERED! (%s:%s:%d)\n\"%s\"\n", file_name, func_name, line_num, strerror(errno));
     exit(1);
   }
-  INTERNAL void __ebp_debugger(char const *func_name, int line_num)
+  INTERNAL void __ebp_debugger(char const *file_name, char const *func_name, int line_num)
   { 
     char *errno_str = strerror(errno);
     return;
   }
-  GLOBAL void (*__ebp)(char const *, int) = __ebp_non_debugger;
+  GLOBAL void (*__ebp)(char const *, char const *, int) = __ebp_non_debugger;
 
-  #define BP() __bp(__func__, __LINE__)
-  #define EBP() __ebp(__func__, __LINE__)
+  #define BP() __bp(__FILE__, __func__, __LINE__)
+  #define EBP() __ebp(__FILE__, __func__, __LINE__)
   #define ASSERT(cond) if (!(cond)) {BP();}
 #else
   #define BP()

1e89923c956f855fa167e48e431bb67fe09f87ea
Ryan McClue <re.mcclue@protonmail.com>
2022-05-29 18:18:17 +1000

    More

--- desktop-template/code/desktop.cpp
+++ desktop-template/code/desktop.cpp
@@ -8,23 +8,14 @@
 #include "desktop.h"
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 INTERNAL void
-update_and_render(BackBuffer *back_buffer, Input *input)
+update_and_render(SDL_Renderer *renderer, Input *input)
 {
-  u32 *pixels = back_buffer->pixels;
+  SDL_RenderClear(renderer);
 
-  for (u32 y = 0;
-       y < back_buffer_dim.h;
-       ++y)
-  {
-    for (u32 x = 0;
-        x < back_buffer_dim.w;
-        ++x)
-    {
-      *pixels++ = 0xff0000ff;     
-    }
-  }
+  SDL_RenderPresent(renderer);
 }
 
 int
@@ -34,7 +25,21 @@ main(int argc, char *argv[])
 
   if (SDL_Init(SDL_INIT_VIDEO) == 0)
   {
-    V2 window_dim = {1280, 720};
+    if (TTF_Init() == 0)
+    {
+
+    }
+    else
+    {
+      BP_MSG(TTF_GetError()); 
+    }
+
+int
+main(int argc, char *argv[])
+{
+  if (SDL_Init(SDL_INIT_VIDEO) == 0)
+  {
+    V2u window_dim = {1280, 720};
     SDL_Window *window = SDL_CreateWindow("Name", SDL_WINDOWPOS_CENTERED, 
                           SDL_WINDOWPOS_CENTERED, window_dim.w, window_dim.h, 
                           SDL_WINDOW_RESIZABLE);
@@ -47,61 +52,112 @@ main(int argc, char *argv[])
 
         // IMPORTANT(Ryan): Mouse values line up to this logical width and height
         SDL_RenderSetLogicalSize(renderer, window_dim.w, window_dim.h);
-        SDL_RenderSetIntegerScale(renderer, 1);
+        SDL_RenderSetIntegerScale(renderer, SDL_TRUE);
 
-        V2 back_buffer_dim = window_dim;
+        V2u back_buffer_dim = window_dim;
         SDL_Texture *back_buffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, 
                                                              SDL_TEXTUREACCESS_STREAMING,
                                                              back_buffer_dim.w, 
                                                              back_buffer_dim.h);
         if (back_buffer_texture != NULL)
         {
-          u32 *back_buffer_pixels = malloc(back_buffer_dim.w * back_buffer_dim.h * sizeof(u32));
-          if (back_buffer_pixels != NULL)
+          u32 back_buffer_size = back_buffer_dim.w * back_buffer_dim.h * sizeof(u32);
+          u32 memory_size = GIGABYTES(1);
+          void *mem = calloc(back_buffer_size + memory_size, 1);
+          if (mem != NULL)
           {
             BackBuffer back_buffer = {};
-            back_buffer.texture = back_buffer_texture;
             back_buffer.dim = back_buffer_dim;
+            back_buffer.pixels = (u32 *)mem;
 
+            // TODO(Ryan): Will have to call again if in fullscreen mode
             Input input = {};
+            u32 refresh_rate = get_refresh_rate(window);
+            input.update_dt = 1.0f / (r32)refresh_rate;
+            
+            Memory memory = {};
+            memory.size = memory_size;
+            memory.mem = (u8 *)mem + back_buffer_size;
 
-            bool want_to_run = true;
-            while (want_to_run)
+            Functions functions = {};
+            functions.read_entire_file = read_entire_file;
+            functions.free_file_result = free_file_result;
+
+            LoadableUpdateAndRender loadable_update_and_render = {};
+            loadable_update_and_render.base_file = "/home/ryan/prog/personal/gui/run/gui.so";
+            loadable_update_and_render.toggle_file = "/home/ryan/prog/personal/gui/run/gui.so.toggle";
+
+            UpdateAndRender current_update_and_render = \
+              reload_update_and_render(&loadable_update_and_render);
+            if (current_update_and_render != NULL)
             {
-              SDL_Event event = {};
-              while (SDL_PollEvent(&event))
-              {
-                if (event.type == SDL_QUIT)
-                {
-                  want_to_run = false;
-                }
-              }
+              DebugFrameEndInfo debug_info = {};
 
-              SDL_RenderClear(renderer);
+              u64 last_counter = SDL_GetPerformanceCounter(); 
 
-              u32 pitch = 0;
-              if (SDL_LockTexture(back_buffer_texture, NULL, (void **)&back_buffer->pixels, &pitch) != 0)
+              bool want_to_run = true;
+              while (want_to_run)
               {
-                update_and_render(&back_buffer, &input);
+                // FRAME_MARKER();
                 
-                SDL_UnlockTexture(back_buffer_texture);
+                SDL_Event event = {};
+                // BEGIN_BLOCK(EventProcessing);
+                while (SDL_PollEvent(&event))
+                {
+                  if (event.type == SDL_QUIT)
+                  {
+                    want_to_run = false;
+                  }
+                }
+                // END_BLOCK();
 
-                SDL_RenderCopy(renderer, back_buffer_texture, NULL, NULL);
+                debug_info.events_processed = get_elapsed_seconds(last_counter, 
+                                                                  SDL_GetPerformanceCounter());
+
+                SDL_RenderClear(renderer);
+
+                current_update_and_render = \
+                  reload_update_and_render(&loadable_update_and_render);
+
+                s32 pitch = 0;
+                if (SDL_LockTexture(back_buffer_texture, NULL, (void **)&back_buffer.pixels, 
+                      &pitch) == 0)
+                {
+                  current_update_and_render(&back_buffer, &input, &memory, &functions);
+
+                  SDL_UnlockTexture(back_buffer_texture);
+
+                  SDL_RenderCopy(renderer, back_buffer_texture, NULL, NULL);
+                }
+                else
+                {
+                  BP_MSG(SDL_GetError());
+                }
+
+                SDL_RenderPresent(renderer);
+
+                u64 end_counter = SDL_GetPerformanceCounter();
+                last_counter = end_counter;
+
+                debug_info.end_of_frame = get_elapsed_seconds(last_counter, end_counter);
+
+                // if (game_functions->debug_frame_end)
+                // {
+                // IMPORTANT(Ryan): No longer need this, as store in globals
+                // game_functions->debug_frame_end(&memory, &debug_info)
+                // };
               }
-              else
-              {
-                BP_MSG(SDL_GetError());
-              }
-      
-              SDL_RenderPresent(renderer);
+            }
+            else
+            {
+              BP_MSG(SDL_GetError());
+              SDL_Quit();
+              return 1;
             }
           }
           else
           {
             EBP();
-            SDL_DestroyTexture(back_buffer_texture);
-            SDL_DestroyRenderer(renderer);
-            SDL_DestroyWindow(window);
             SDL_Quit();
             return 1;
           }
@@ -109,8 +165,6 @@ main(int argc, char *argv[])
         else
         {
           BP_MSG(SDL_GetError());
-          SDL_DestroyRenderer();
-          SDL_DestroyWindow();
           SDL_Quit();
           return 1;
         }
@@ -118,7 +172,6 @@ main(int argc, char *argv[])
       else
       {
         BP_MSG(SDL_GetError());
-        SDL_DestroyWindow();
         SDL_Quit();
         return 1;
       }
@@ -135,6 +188,14 @@ main(int argc, char *argv[])
     BP_MSG(SDL_GetError());
     return 1;
   }
+
+  SDL_Quit();
+
+  return 0;
+}
+    
+
+// TTF_Init();
 
   SDL_DestroyTexture(back_buffer_texture);
   SDL_DestroyRenderer(renderer);
